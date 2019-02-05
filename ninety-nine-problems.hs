ef9daa35c6da38649f6db07a7030cf9b5e352f9f
John McEntee <john.mcentee@semanticbits.com>
2019-02-05 14:39:45 -0500

    #2

--- ninety-nine-problems.hs
+++ ninety-nine-problems.hs
@@ -1,5 +1,5 @@
 -- 1) Find the last element of a list
-myLast :: [Int] -> Int
+myLast :: [a] -> a
 myLast [] = error "No last for empty list"
 myLast [x] = x
 myLast (_:xs) = myLast xs
@@ -7,3 +7,7 @@ myLast (_:xs) = myLast xs
 myLast' = head . reverse
 
 -- 2) Find the last but one element of a list
+myLastButOne :: [a] -> a
+myLastButOne = last . init
+
+-- 3) Find the K'th element of a list. The first element in the list is number
